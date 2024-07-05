3832110ae8f828efd9e31cad733304362455a630
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-07-05 22:32:51 +0530

    Time: 160 ms (77.92%), Space: 119.8 MB (11.59%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,24 +11,36 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+
+        ListNode* temp = head;
+        ListNode* prev = head;
+        temp = temp->next;
+        // prev = prev->next;
+        vector<int> critical_point_arr;
+        // vector<int> resultant;
+        int position_count = 1;
         if (!head || !head->next || !head->next->next) return {-1, -1};
 
-        ListNode* temp = head->next;
-        ListNode* prev = head;
-        vector<int> critical_point_arr;
-        int position_count = 1;
-        
-        while(temp != nullptr && temp->next != nullptr) {
-            if((temp->val > prev->val && temp->val > temp->next->val) || (temp->val < prev->val && temp->val < temp->next->val)) {
+        while(temp != nullptr && temp->next != nullptr)
+        {
+            if((temp->next->val > temp->val && prev->val > temp->val) || (temp->next->val < temp->val && prev->val < temp->val))
+            {
                 critical_point_arr.push_back(position_count);
             }
             prev = temp;
             temp = temp->next;
+            
             position_count++;
         }
-        
+        // if(position_count < 3) return '[-1, -1]';
         if(critical_point_arr.size() < 2) return {-1, -1};
-        
+
+        // int min = critical_point_arr[critical_point_arr.size() - 1] - critical_point_arr[critical_point_arr.size() - 2];
+        // int max = critical_point_arr[critical_point_arr.size() - 1] - critical_point_arr[0];
+        // // resultant.push_back(min);
+        // // resultant.push_back(max);
+
+        // return {min, max};
         int min_dist = INT_MAX;
         for(int i = 1; i < critical_point_arr.size(); i++) {
             min_dist = min(min_dist, critical_point_arr[i] - critical_point_arr[i - 1]);
@@ -37,5 +49,6 @@ public:
         int max_dist = critical_point_arr.back() - critical_point_arr.front();
         
         return {min_dist, max_dist};
+        
     }
 };
