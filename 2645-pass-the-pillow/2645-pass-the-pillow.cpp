dbb190bb4daf631ec550a393858af9d254911b13
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-07-06 09:42:50 +0530

    Time: 0 ms (100%), Space: 7.2 MB (30.58%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -3,9 +3,10 @@ public:
     int passThePillow(int n, int time) {
 
         int round_no = 0, remaining_time = time;
+
         if(time == 0) return time;
         else if(time < n) return time + 1;
-        int pillow_position = 1;
+
         while(true)
         {
             if(remaining_time > n - 1) {
@@ -13,16 +14,13 @@ public:
                 round_no++;
                 continue;
             }
-            // return remaining_time;
+            
             if(round_no % 2 == 0) {
                 return remaining_time + 1;
             }
-            else if(round_no % 2 == 1) {
+            else {
                 return (n - remaining_time);
             }
-            
-        }
-        return remaining_time;
-        
+        }        
     }
 };
