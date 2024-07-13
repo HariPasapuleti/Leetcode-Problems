3c1f876db62ed8369d4c53bafb31b0f00350044f
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-07-13 21:04:36 +0530

    Time: 85 ms (82.88%), Space: 62 MB (40.69%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+
+        sort(nums.begin(), nums.end());
+        int i = 0, j = nums.size() - 1, operations = 0;
+
+        while(i < j)
+        {
+            if(nums[i] + nums[j] == k) 
+            {
+                operations++;
+                i++;
+                j--;
+            }
+            else if(nums[i] + nums[j] < k) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return operations;
+    }
+};
