87a89a374c9d1eb9208b380b70ea949518409734
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-07-17 00:19:29 +0530

    Time: 48 ms (34.72%), Space: 38.9 MB (22.2%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -2,27 +2,37 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         
-        set <int> set1(nums1.begin(), nums1.end());
-        set <int> set2(nums2.begin(), nums2.end());
+        map <int, int> map1;
+        map <int, int> map2;
 
-        // set<int> s;
+        for(auto num : nums1)
+        {
+            map1[num++];
+        }
 
-        // for(auto i : nums2)
-        // {
-        //     if(map1.find(i) != map1.end()) s.insert(i);
-        // }
+        for(auto num : nums2)
+        {
+            map2[num++];
+        }
+
+        set<int> s;
+
+        for(auto i : nums2)
+        {
+            if(map1.find(i) != map1.end()) s.insert(i);
+        }
 
         vector<int> v1;
         vector<int> v2;
 
-        for(auto num : set1)
+        for(auto i : map1)
         {
-            if(set2.find(num) == set2.end()) v1.push_back(num);
+            if(s.find(i.first) == s.end()) v1.push_back(i.first);
         }
 
-        for(auto num : set2)
+        for(auto i : map2)
         {
-            if(set1.find(num) == set1.end()) v2.push_back(num);
+            if(s.find(i.first) == s.end()) v2.push_back(i.first);
         }
         return {v1, v2};
     }
