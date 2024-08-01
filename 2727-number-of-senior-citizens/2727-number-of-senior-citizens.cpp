b595c6dce3c43ca065c69b58fd5b12ef9b0a7d8a
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-08-01 18:15:34 +0530

    Time: 6 ms (60.14%), Space: 17.6 MB (16.71%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -4,16 +4,18 @@ public:
 
         int age, old_passengers = 0;
         string age_str = "";
-        for (int i = 0; i < details.size(); i++) {
+        for(int i=0;i<details.size();i++)
+        {
             string age_str = "";
             age_str += details[i][11];
             age_str += details[i][12];
 
             age = stoi(age_str);
-            if (age > 60) {
+            if(age > 60) {
                 old_passengers++;
             }
         }
         return old_passengers;
+        
     }
 };
