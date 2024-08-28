502927d011a1e188f05ae9d8350beb684d77ebbd
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-08-28 23:10:18 +0530

    Time: 501 ms (88.41%), Space: 0B (100%) - LeetHub

---
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+
+with temp as (
+    select player_id, min(event_date) as first_login_date
+    from activity
+    group by player_id
+)
+
+select 
+    round(
+            sum(datediff(a.event_date, t.first_login_date) = 1)
+            /
+            count(distinct a.player_id), 2)
+    as fraction
+from 
+    activity as a
+join 
+    temp as t 
+    on a.player_id = t.player_id
+
+
