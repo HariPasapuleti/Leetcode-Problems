1ddbcd3b61a06207950d99f27f00b98203206359
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-08-30 14:52:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (34.45%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -3,20 +3,21 @@ public:
     vector<int> selfDividingNumbers(int left, int right) {
 
         vector<int> res;
-        for (int i = left; i <= right; i++) {
-            int num = i, flag = 0;
-            while (num > 0) {
-                flag = 0;
-                int digit = num % 10;
-                if (digit == 0 || i % digit != 0) {
-                    flag = 1;
+        for(int i=left;i<=right;i++)
+        {
+            int num=i,flag=0;
+            while(num>0) 
+            {
+                flag=0;
+                int digit=num%10;
+                if(digit==0 || i%digit!=0) {
+                    flag=1;
                     break;
                 };
-                num /= 10;
+                num/=10;
             }
-            if (flag == 0)
-                res.push_back(i);
+            if(flag==0) res.push_back(i);
         }
-        return res;
+        return res;   
     }
 };
