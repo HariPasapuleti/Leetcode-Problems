9b03d393fcb154eaf31f3504c13243e3802d0ecf
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-09-09 12:12:20 +0530

    Time: 218 ms (91.36%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,20 @@
 # Write your MySQL query statement below
 
-select (select distinct salary  from employee 
-order by salary desc
-limit 1, 1) as SecondHighestSalary;
+select
+    case
+        when count(distinct salary) > 1 then 
+            (
+                select distinct
+                    salary
+                from
+                    employee
+                order by
+                    salary desc
+                limit 1 offset 1
+            )
+        else 
+            NULL
+    end as SecondHighestSalary
+from 
+    employee
 
