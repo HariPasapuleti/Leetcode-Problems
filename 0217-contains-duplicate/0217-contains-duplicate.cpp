9bdf13aba147bddc5edead9dfb080bb41f212a60
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-10-02 15:51:13 +0530

    Time: 105 ms (29.53%), Space: 75.5 MB (18.33%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_map <int,int> mp;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+
+        for(auto num : mp){
+            if(num.second > 1) return true;
+        }
+        return false;
+        
+    }
+};
