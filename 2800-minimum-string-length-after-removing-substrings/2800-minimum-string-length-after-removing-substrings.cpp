85cb08c17f3d08942385c5fd96163e1e40cc9b67
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-10-07 21:54:33 +0530

    Time: 8 ms (42.58%), Space: 11.6 MB (61.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int minLength(string s) {
 
-        int n = s.size(), i = 0;
-        while (i < s.size() - 1) {
-            if ((i != n - 1) && (s[i] == 'A' && s[i + 1] == 'B') ||
-                (s[i] == 'C' && s[i + 1] == 'D')) {
-                s.erase(i, 2);
-                if (s.size() == 0)
-                    return 0;
-                if (i > 0)
-                    i--;
-            } else
-                i++;
+        stack<char> stk;
+
+        int i = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (!stk.empty() && ((stk.top() == 'A' && s[i] == 'B') ||
+                                 (stk.top() == 'C' && s[i] == 'D')))
+                stk.pop();
+            else
+                stk.push(s[i]);
         }
-        return s.size();
+
+        return stk.size();
     }
 };
