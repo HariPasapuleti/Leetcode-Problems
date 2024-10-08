03ae9aa7c85282bafa4a53471739e677fb321f5a
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-10-08 12:41:04 +0530

    Time: 3 ms (75.81%), Space: 16.3 MB (14.08%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,89 +1,33 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    std::vector<std::vector<int>> combinationSum2(std::vector<int>& candidates, int target) {
-        std::vector<std::vector<int>> result;
-        std::vector<int> combination;
-        std::sort(candidates.begin(), candidates.end()); // Sort the candidates to handle duplicates
-        backtrack(candidates, target, 0, combination, result);
-        return result;
+    vector<vector<int>> combinationSum2(vector<int>& canditates, int target) {
+
+        int n = canditates.size();
+        vector<vector<int>> res;
+        vector<int> part;
+        sort(canditates.begin(), canditates.end());
+        helper(0, canditates, target, res, part);
+
+        return res;
     }
 
-private:
-    void backtrack(const std::vector<int>& candidates, int target, int start,
-                   std::vector<int>& combination, std::vector<std::vector<int>>& result) {
+    void helper(int index, vector<int> canditates, int target,
+                vector<vector<int>>& res, vector<int>& part) {
+
         if (target == 0) {
-            result.push_back(combination);
+            res.push_back(part);
             return;
         }
+        
+        for(int i=index;i<canditates.size();i++)
+        {
+            if(i>index && canditates[i]==canditates[i-1]) continue;
+            if(target<canditates[i]) break;
 
-        for (int i = start; i < candidates.size(); ++i) {
-            if (i > start && candidates[i] == candidates[i - 1]) {
-                // Skip duplicates
-                continue;
-            }
-
-            if (candidates[i] > target) {
-                // No need to continue if the candidate exceeds the target
-                break;
-            }
-
-            combination.push_back(candidates[i]);
-            backtrack(candidates, target - candidates[i], i + 1, combination, result);
-            combination.pop_back();
+            part.push_back(canditates[i]);
+            helper(i+1,canditates,target-canditates[i],res,part);
+            part.pop_back();
         }
+
     }
 };
-
-
-// class Solution {
-// public:
-//     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-
-//         sort(candidates.begin(), candidates.end());
-
-//         for(int i=0;i<candidates.size();i++)
-//         {
-//             if(arr[i] >= target) {
-//                 j = i;
-//                 break;
-//             }
-//         }
-
-//         int i = 0;
-//         vector <int> res;
-
-//         while(i < j)
-//         {
-//             if(arr[i] + arr[j] == target) {
-//                 res[i].push_back(arr[i]);
-//                 res[i].push_back(arr[j]);
-//                 i++; j--;
-//                 continue;
-//             }
-//             else if(arr[i] + arr[j] < target) {
-//                 int k = i;
-//                 int temp_sum = arr[j];
-//                 res[i].push_back(arr[j]);
-//                 while(temp_sum < target)
-//                 {
-//                     res[i].push_back(arr[i]);
-                    
-//                     temp_sum += arr[i];
-//                     i++;
-//                 }
-                
-//                 if(temp_sum > target) {
-//                     res.erase(res.begin(), i);
-//                     i=k;
-//                     j--;
-//                 }
-                
-//             }
-//             j--;
-//         }
-        
-//     }
-// };
