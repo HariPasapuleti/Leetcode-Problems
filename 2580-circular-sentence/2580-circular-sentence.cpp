708aa6d2a022b752064096231d2e58d41274c83c
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-11-02 23:44:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    bool isCircularSentence(string sentence) {
+    bool isCircularSentence(string s) {
 
-        int i = 0, j = sentence.size() - 1;
-        if (sentence[i] != sentence[j])
+        int n = s.size();
+        if (s[0] != s[n - 1])
             return false;
 
-        while (i < sentence.size()) {
-            if (sentence[i] == ' ') {
-                if (sentence[i - 1] != sentence[i + 1])
-                    return false;
-            }
+        int i = 0;
+        while (i < n) {
+            if (s[i] == ' ' && s[i - 1] != s[i + 1])
+                return false;
             i++;
         }
         return true;
