3b072911699c0967931802a761451ff14499975d
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-11-30 16:52:11 +0530

    Time: 0 ms (100%), Space: 19.9 MB (24.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,20 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
 
-        vector<int> res;
-        unordered_map<int, int> mp;
-
+        int ele1 = INT_MIN, ele2 = INT_MIN, cnt1 = 0, cnt2 = 0;
         for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]]++;
+            if (cnt1 == 0 && ele2 != nums[i]) {
+                cnt1 = 1;
+                ele1 = nums[i];
+            } else if (cnt2 == 0 && ele1 != nums[i]) {
+                cnt2 = 1;
+                ele2 = nums[i];
+            } else if (ele1 == nums[i])
+                cnt1++;
+            else if (ele2 == nums[i])
+                cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == ele1)
+                cnt1++;
+            if (nums[i] == ele2)
+                cnt2++;
         }
 
         int div = nums.size() / 3;
-        cout << div << endl;
-        for (auto num : mp) {
-            cout << num.second << endl;
-            if (num.second > div)
-                res.push_back(num.first);
-        }
+        vector<int> res;
+        if (cnt1 > div)
+            res.push_back(ele1);
+        if (cnt2 > div)
+            res.push_back(ele2);
 
         return res;
     }
