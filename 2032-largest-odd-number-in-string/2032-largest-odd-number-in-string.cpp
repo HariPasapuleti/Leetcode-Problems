f2f3476a8fd3ee8c905722fa2d5aae6f0d6aa845
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2024-12-09 21:44:14 +0530

    Time: 5 ms (13.76%), Space: 19.2 MB (5.29%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        string res = "", temp = "";
-        for (int i = 0; i < num.size(); i++) {
-            temp += num[i];
-            if ((num[i] - '0') % 2 != 0)
-                res = temp;
+        int flag = 0;
+        string res = "";
+        for (int i = num.size() - 1; i >= 0; i--) {
+            if (flag == 1) {
+                res += num[i];
+                continue;
+            }
+            if ((num[i] - '0') % 2 != 0) {
+                flag = 1;
+                res += num[i];
+            }
         }
+        if (res == "")
+            return res;
+        reverse(res.begin(), res.end());
         return res;
     }
 };
