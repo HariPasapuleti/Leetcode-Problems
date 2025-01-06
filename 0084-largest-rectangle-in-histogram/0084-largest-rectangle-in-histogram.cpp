9b22a93fe836521aadc9cd1195a196dacbaeb3d1
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-01-06 22:31:21 +0530

    Time: 2575 ms (5%), Space: 97.9 MB (5.02%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> nse(n, 0), pse(n, 0);
+        nse = nsee(heights);
+        pse = psee(heights);
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            cout << pse[i] << " " << nse[i] << endl;
+            total = max(total, (nse[i] - pse[i] - 1) * heights[i]);
+        }
+        return total;
+    }
+
+    vector<int> psee(vector<int> heights) {
+        vector<int> suffix(heights.size(), 0);
+        stack<int> st;
+        for (int i = 0; i < heights.size(); i++) {
+            while (!st.empty() && heights[i] <= heights[st.top()])
+                st.pop();
+            suffix[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return suffix;
+    }
+
+    vector<int> nsee(vector<int> heights) {
+        vector<int> prefix(heights.size(), 0);
+        stack<int> st;
+        // prefix[heights.size() - 1] = heights[heights.size() - 1];
+        for (int i = heights.size() - 1; i >= 0; i--) {
+            while (!st.empty() && heights[i] <= heights[st.top()])
+                st.pop();
+            prefix[i] = st.empty() ? heights.size() : st.top();
+            st.push(i);
+        }
+        return prefix;
+    }
+};
