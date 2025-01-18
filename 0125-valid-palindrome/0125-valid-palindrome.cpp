bfad3ac9634fee3d520c24e3bfa08642e1788baa
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-01-18 22:20:47 +0530

    Time: 0 ms (100%), Space: 10.1 MB (35.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,23 +3,13 @@ public:
     bool isPalindrome(string s) {
         int i = 0, j = s.size() - 1;
         while (i < j) {
-            while (i < s.size() && !isalpha(s[i]) && !isdigit(s[i]))
-                i++;
-            while (j > 0 && !isalpha(s[j]) && !isdigit(s[j]))
-                j--;
-            if (isalpha(s[i]) && isalpha(s[j]) &&
-                (s[i] - 'a' == s[j] - 'a' || s[i] - 'A' == s[j] - 'A' ||
-                 s[i] - 'a' == s[j] - 'A' || s[i] - 'A' == s[j] - 'a')) {
-                i++;
-                j--;
-            } else if (s[i] - '0' == s[j] - '0') {
-                i++;
-                j--;
-            } else {
-                if ((isalpha(s[i]) || isdigit(s[i])) &&
-                    (isalpha(s[j]) || isdigit(s[j])))
-                    return false;
+            if (!isalnum(s[i])) { i++; continue;}
+            if (!isalnum(s[j])) {j--;continue;}
+            if (tolower(s[i]) != tolower(s[j])) {
+                return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
