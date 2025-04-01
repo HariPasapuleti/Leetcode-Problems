4d764c7a3f01f584f66716f49b13d77b7cfdcca7
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-04-01 11:05:25 +0530

    Time: 8 ms (49.73%), Space: 127.4 MB (36.85%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long> dp(n, -1);
+        return rec(0, questions, dp, n);
+    }
+
+    long rec(int i, vector<vector<int>>& questions, vector<long>& dp, int n) {
+
+        if (i >= n)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+
+        long take =
+            questions[i][0] + rec(i + questions[i][1] + 1, questions, dp, n);
+        long notTake = rec(i + 1, questions, dp, n);
+        return dp[i] = max(take, notTake);
+    }
+};
