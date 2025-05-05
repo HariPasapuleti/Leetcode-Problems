e14aed243d06dc2fe213b2872144bb23f5e22aa5
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-05-05 12:56:39 +0530

    Time: 0 ms (100%), Space: 8.1 MB (72.14%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int numTilings(int n) {
         vector<long long> dp(1001);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
+        dp[0] = 0, dp[1] = 1, dp[2] = 2, dp[3] = 5;
         if (n <= 3)
             return dp[n];
         for (int i = 4; i <= n; i++) {
