d6b9b41414b47445612091fffbc5169ae0f313c5
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-05-27 09:37:57 +0530

    Time: 0 ms (100%), Space: 8.4 MB (36.04%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int div_sum=0, non_div_sum=0;
-        for(int i=1;i<=n;i++) {
-            if(i%m==0) {
-                div_sum+=i;
-            }
-            else {
-                non_div_sum+=i;
-            }
-        }
-        return non_div_sum-div_sum;
+        int total_sum = n * (n + 1) / 2, m_divisible = n / m;
+        int m_divisible_sum = m_divisible * (m_divisible + 1) / 2;
+        return total_sum - (2 * m_divisible_sum * m);
     }
 };
