8fe6cc8014798c02e0d5b534aca24b52b04ed42e
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-07-24 11:03:48 +0530

    Time: 0 ms (100%), Space: 9.1 MB (36.82%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = s[i - 1] - '0';
+            int twoDigits = stoi(s.substr(i - 2, 2));
+            if (oneDigit > 0) {
+                dp[i] += dp[i - 1];
+            }
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+};
