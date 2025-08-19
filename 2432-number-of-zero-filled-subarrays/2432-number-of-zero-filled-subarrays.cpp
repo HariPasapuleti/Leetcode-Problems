d3927431cda876bd289f769067a39362ef2eb96b
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-08-19 11:21:57 +0530

    Time: 0 ms (100%), Space: 111.5 MB (11.1%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -4,13 +4,14 @@ public:
         long long res = 0, zero_count = 0;
         for (int right = 0; right < nums.size(); right++) {
             if (nums[right] == 0) {
+                res += zero_count + 1;
                 zero_count++;
             } else {
-                res += zero_count * (zero_count + 1) / 2;
+                // res += zero_count * (zero_count + 1) / 2;
                 zero_count = 0;
             }
         }
-        res += zero_count * (zero_count + 1) / 2;
+        // res += zero_count * (zero_count + 1) / 2;
         return res;
     }
 };
