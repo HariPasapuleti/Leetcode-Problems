ef83e06360efd798cd8301b362f0938c4db35e82
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-08-24 08:56:48 +0530

    Time: 0 ms (100%), Space: 59.9 MB (98.72%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-
-        int zeroes_count = 0, i = 0, j = 0, ones_count = 0;
-
-        while(i < nums.size())
-        {
-            if(nums[i] == 0) {
-                zeroes_count++;
+        int left = 0, res = 0, zero_count = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            zero_count += (nums[right] == 0);
+            while (zero_count > 1) {
+                if (nums[left] == 0)
+                    zero_count--;
+                left++;
             }
-            if(zeroes_count > 1) {
-                while(nums[j++] != 0);
-                zeroes_count--;
-            }
-            ones_count = max(ones_count, i - j);
-            i++;
+            res = max(res, right - left);
         }
-        return ones_count;
-        
-        // int i = 0, ones_count = 0, zeroes_count = 0, max_ones = 0;
-        // while(i < nums.size())
-        // {
-        //     if(nums[i] == 1) {
-        //         ones_count++;
-        //     }
-        //     else {
-        //         if(zeroes_count < 1) {
-        //             zeroes_count++;
-        //         }
-        //         else if(nums[i - ones_count - 1] == 0) {
-        //             zeroes_count = 1;
-        //         }
-        //         else {
-        //             ones_count = 0;
-        //         }
-                
-        //     }
-        //     cout << ones_count << endl;
-        //     max_ones = max(max_ones, ones_count);
-        //     i++;
-        // }
-        // if(max_ones == nums.size()) max_ones--;
-        // return max_ones;
+        return res;
     }
 };
