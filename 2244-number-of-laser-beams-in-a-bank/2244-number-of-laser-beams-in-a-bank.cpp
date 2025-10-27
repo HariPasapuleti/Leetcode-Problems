259baf903ba7353ff3e03e57d6693f60f996d46d
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-10-27 13:13:45 +0530

    Time: 3 ms (84.39%), Space: 27.3 MB (84.17%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-
-        // string bank;
-        int result_count = 0, prev_count = 0;
-        
-        for(int i = 0; i < bank.size(); i++)
-        {
-            string current_binary = bank[i];
-            // if(bank[i+1]) string next_binary = bank[i+1];
-            int current_count = 0;
-
-            for(char c : current_binary)
-            {
-                if(c == '1') current_count++;
+        int res = 0, beams = 0;
+        for (int i = 0; i < bank.size(); i++) {
+            int temp_beams = 0;
+            for (auto block : bank[i]) {
+                if (block == '1') {
+                    temp_beams++;
+                }
             }
-
-            if(i != 0)
-            {
-                result_count += current_count * prev_count;
+            if (beams != 0) {
+                res += beams * temp_beams;
             }
-            if(current_count > 0) prev_count = current_count;
-            
-
+            if (temp_beams != 0) {
+                beams = temp_beams;
+            }
         }
-        return result_count;
+        return res;
     }
 };
