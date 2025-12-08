2d5548820acbf4be661b3a6d79b8d998d3838d33
Hari Veera Venkat Pasapuleti <144843244+HariPasapuleti@users.noreply.github.com>
2025-12-08 10:47:52 +0530

    Time: 7 ms (62.17%), Space: 7.8 MB (84.51%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int countTriples(int n) {
-        vector<bool> sqr(n * n + 1, false);
-        for (int i = 0; i <= n; i++) {
-            sqr[i * i] = true;
-        }
         int count = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 int sum = i * i + j * j;
-                if (sum <= n * n && sqr[sum]) {
+                int k = int(sqrt(sum + 1));
+                if (k <= n && k * k == sum) {
                     count++;
                 }
             }
